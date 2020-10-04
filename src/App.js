f943d95d5579a7c67b225871fb75c29b78336c79
Kazim Muhammad <kazmodeston1@gmail.com>
2020-10-04 08:19:25 +0100

    working on State by importing useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import TweetComponent from './TweetComponent'
+import React,{useState} from 'react';
+import "./App.css"
 function App(){
 
+  // isRed is the actual variable name, while setRed is the function whenever we want to change the value of isRed
+ const [isGreen, setGreen] = useState(false);
+ const [count, setCount] = useState(0);
+
+ const increment = () =>{
+  setCount(count+1);
+  setGreen(!isGreen)
+ }
 
   return (
   <div className="app">
-      <TweetComponent name="Damilare" tweet="Leave Vue and start React" />
-      <TweetComponent name="Janet" tweet="Before u know, U will reach our level" />
-      <TweetComponent name="Tijani" tweet="Who Am I" />
-      <TweetComponent name="Steven" tweet="Whisky and CSS" />
+    <h2 className={isGreen ? 'green':'black'}>Change Color to Green</h2>
+     <button className="buttonEvent" onClick={increment}>Increment</button>
+     <h2>{count}</h2>
   </div>
 
   );
